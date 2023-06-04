bdfa50c2698570f6feba7f7431f3ba00b3d67ac3
student <student@VTC5-PC>
2023-06-04 19:49:07 +0400

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+             for (int i=50;i<=70;i++)
+            {
+                int b = 0;
+                b = i;
+                do
+                {
+                    if (b % 2 == 0)
+                    {
+                        b = b / 2;
+
+                    }
+                    else
+                    {
+                        b = (b * 3 + 1) / 2;
+                    }
+                }
+                while (b != 1);
+                Console.WriteLine(b);
+                
+
+            }
+
+
+        }
+    }
+}
